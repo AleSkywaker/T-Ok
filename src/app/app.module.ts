7e7898cc98ce509552c79458d2aab9abad3845fc
Alex Colombo <uricaine@hotmail.com>
2019-08-01 09:09:36 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Modulos de terceros
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { GraficoD3Component } from './pages/grafico-d3/grafico-d3.component';
 import { GraficaAmComponent } from './pages/grafica-am/grafica-am.component';
 import { MapasComponent } from './pages/mapas/mapas.component';
 import { GoogleChartsComponent } from './pages/google-charts/google-charts.component';
-import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 import { TimelineComponent } from './pages/timeline/timeline.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DatePickerComponent } from './shared/date-picker/date-picker.component';
 import { FormularioComponent } from './shared/formulario/formulario.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +45,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     Ng2GoogleChartsModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   exports: [],
   providers: [],
