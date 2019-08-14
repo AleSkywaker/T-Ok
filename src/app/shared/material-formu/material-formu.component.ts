e969bf8bae57f3162fdd5ab1ab372b8e9e270fa8
Alex Colombo <uricaine@hotmail.com>
2019-08-14 11:22:08 +0200

    new component added

---
+++ src/app/shared/material-formu/material-formu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-material-formu',
+  templateUrl: './material-formu.component.html',
+  styleUrls: ['./material-formu.component.css']
+})
+export class MaterialFormuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
